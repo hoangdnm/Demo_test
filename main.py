306340e6c5e0cf231716ded1bf9c4e8840f35501
hoangdnm <daongocminhhoang20032004@gmail.com>
2025-07-08 16:51:18 +0700

    1

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ processes = []
 # Khởi động tất cả tiến trình trong lần lặp này
 for file in files:
     print(f"Đang chạy {file}...")
-    p = subprocess.Popen(["python", file])
+    p = subprocess.Popen(["python3", file])#sử dụng python3 để đảm bảo chạy trên môi trường Python 3
+    time.sleep(1)  # Đợi 1 giây trước khi khởi động
     processes.append(p)
 # Chờ tất cả tiến trình trong lần lặp này hoàn tất
 for p in processes:
